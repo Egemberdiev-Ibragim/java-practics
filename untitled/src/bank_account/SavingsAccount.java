82c3c422c70cd04d23b57bb7bab934e3d1ce065d
Egemberdiev-Ibragim <inthistoru@gmail.com>
2023-12-03 15:54:02 +0600

    Added functionality for managing bank accounts: credit transfers and transaction processing optimization

--- untitled/src/bank_account/SavingsAccount.java
+++ untitled/src/bank_account/SavingsAccount.java
@@ -1,11 +1,10 @@
 package bank_account;
 
-public class SavingsAccount extends Account {
+public class SavingsAccount extends Account{
     @Override
     public String getAccountType() {
         return "Сберегательный счет!";
     }
-
     @Override
     public void pay(int amount) {
         System.out.println("Оплата невозможна, так как это сберегательный счет.");
